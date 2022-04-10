bcd898699b088d31f2608ef996ba9a4cef4b9d56
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-04-10 14:21:48 -0500

    initial commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,38 @@
+async function main() {
 
-// Your Code Here
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.getElementById('root');
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let input = document.createElement('input')
+    input.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+    })
+
+
+    li.append(input, saveButton)
+    root.appendChild(li)
+}
+main()
